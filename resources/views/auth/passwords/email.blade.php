f3771386201ef6295a3c7ab1e4ec7a0162278c02
jhs512 <jangka512@gmail.com>
2022-02-06 23:45:18 +0900

    패스워드 재설정을 위한 이메일 발송페이지에 부트스트랩 카드 디자인 적용

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,49 +1,43 @@
 @extends('app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
+    <section class="section-1 t-mt-4 t-flex-grow t-flex t-items-center t-justify-center">
+        <div class="t-w-full t-max-w-lg t-px-4">
+            <div class="card">
+                <div class="card-header"><i class="fas fa-lock-open"></i> {{ __('Reset Password') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-                        <form method="POST" action="{{ route('password.email') }}">
-                            @csrf
+                    <form class="t-grid t-grid-cols-1 t-gap-4 t-mt-4" method="POST" action="{{ route('password.email') }}">
+                        @csrf
 
-                            <div class="row mb-3">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div>
+                            <label class="form-label">{{ __('Email Address') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
 
-                            <div class="row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Send Password Reset Link') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-lock-open"></i>
+                                {{ __('Send Password Reset Link') }}
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
